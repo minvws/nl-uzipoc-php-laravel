29403e84a5a02228a5d5659f2747ad1c28e1f463
Rick Lambrechts <rick@rl-webdiensten.nl>
2023-03-28 22:43:33 +0200

    Use laravel oidc package

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Http\Responses\LoginResponse;
 use Illuminate\Support\ServiceProvider;
+use MinVWS\OpenIDConnectLaravel\Http\Responses\LoginResponseInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(LoginResponseInterface::class, LoginResponse::class);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Services\Oidc\OidcService;
 use Illuminate\Support\Facades\Route;
-use Jumbojett\OpenIDConnectClient;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,45 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', function() {
-    $oidc = new OpenIDConnectClient(provider_url: config('oidc.issuer'));
-    $oidc->setClientID(config('oidc.client_id'));
-    if (!empty(config('oidc.client_secret'))) {
-        $oidc->setClientSecret(config('oidc.client_secret'));
-    }
-    $oidc->setCodeChallengeMethod('S256');
-    $oidc->setRedirectURL(route('login'));
-
-    foreach (config('oidc.additional_scopes') as $scope) {
-        $oidc->addScope($scope);
-    }
-
-    // Redirect to login at max
-    $oidc->authenticate();
-
-    // After login, this is executed
-    // Currently we cannot use the $oidc->requestUserInfo() method because we use JWE
-
-    // Get the access token
-    $accessToken = $oidc->getAccessToken();
-
-    // Custom OIDC service to request user info
-    $oidcService = new OidcService(
-        issuer: config('oidc.issuer'),
-        decryptionKeyPath: config('oidc.decryption_key_path'),
-    );
-
-    // Get user information
-    $userInfo = $oidcService->requestUserInfo($accessToken);
-
-    return redirect()
-        ->route('user')
-        ->with('user', $userInfo);
-})->name('login');
-
 Route::get('/user', function() {
     if (!session()->has('user')) {
-        return redirect()->route('login');
+        return redirect()->route('oidc.login');
     }
 
     dump(session('user'));
