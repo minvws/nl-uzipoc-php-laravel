14c90a71b84a1bd49d630960efe1619224e67a4c
Rick Lambrechts <rick.lambrechts@hotmail.com>
2024-02-09 11:13:11 +0100

    Use laravel oidc package (#15)
    * Use laravel oidc package
    
    * Add secrets folder
    
    * Use new package
    
    * Update description of config block

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Http\Responses\OidcLoginResponseHandler;
 use Illuminate\Support\ServiceProvider;
+use MinVWS\OpenIDConnectLaravel\Http\Responses\LoginResponseHandlerInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(LoginResponseHandlerInterface::class, OidcLoginResponseHandler::class);
     }
 
     /**
--- config/oidc.php
+++ config/oidc.php
@@ -3,10 +3,87 @@
 declare(strict_types=1);
 
 return [
+    /**
+     * The issuer URL of the OpenID Connect provider.
+     */
     'issuer' => env('OIDC_ISSUER', ''),
+
+    /**
+     * The unique identifier assigned to your application
+     * by the OpenID Connect provider.
+     */
     'client_id' => env('OIDC_CLIENT_ID', ''),
+
+    /**
+     * If needed, the client secret that you received
+     * from the OpenID Connect provider.
+     */
     'client_secret' => env('OIDC_CLIENT_SECRET', ''),
+
+    /**
+     * Only needed when response of user info endpoint is encrypted.
+     * This is the path to the JWE decryption key.
+     *
+     * You could add multiple decryption key paths comma separated.
+     */
     'decryption_key_path' => env('OIDC_DECRYPTION_KEY_PATH', ''),
-    'additional_scopes' => explode(',', env('OIDC_ADDITIONAL_SCOPES', '')),
-    'private_key_jwt' => env('OIDC_PRIVATE_KEY_PATH_FOR_JWT', ''),
+
+    /**
+     * By default, the openid scope is requested. If you need additional scopes, you can specify them here.
+     */
+    'additional_scopes' => array_filter(explode(',', env('OIDC_ADDITIONAL_SCOPES', ''))),
+
+    /**
+     * Code Challenge Method used for PKCE.
+     */
+    'code_challenge_method' => env('OIDC_CODE_CHALLENGE_METHOD', 'S256'),
+
+    /**
+     * Configuration Cache
+     */
+    'configuration_cache' => [
+        /**
+         * The cache store to use.
+         */
+        'store' => env('OIDC_CONFIGURATION_CACHE_DRIVER', 'file'),
+
+        /**
+         * The cache TTL in seconds.
+         */
+        'ttl' => env('OIDC_CONFIGURATION_CACHE_TTL', 60 * 60 * 24),
+    ],
+
+    /**
+     * Route configuration
+     */
+    'route_configuration' => [
+        /**
+         * Enable or disable the login route.
+         */
+        'enabled' => env('OIDC_LOGIN_ROUTE_ENABLED', true),
+
+        /**
+         * The url of the login route.
+         */
+        'login_route' => env('OIDC_LOGIN_ROUTE', '/oidc/login'),
+
+        /**
+         * The middleware that runs on the login route.
+         */
+        'middleware' => [
+            'web'
+        ],
+
+        /**
+         * The prefix of the login route.
+         */
+        'prefix' => '',
+    ],
+
+    /**
+     * TLS Verify
+     * Can be disabled for local development.
+     * Is used in OpenIDConfigurationLoader and in the ServiceProvider for OpenIDConnectClient.
+     */
+    'tls_verify' => env('OIDC_TLS_VERIFY', true),
 ];
